3dc002ae73c101e47c42ab81d175a286bb51729a
~STR~ <yttest.testing@gmail.com>
2024-12-11 22:38:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ function closeSeason() {
     page.style.display = 'none';
   });
 }
+const container = document.querySelector('.container');
+const indicator = document.getElementById('timeline-indicator');
+const sections = document.querySelectorAll('.section');
+
+container.addEventListener('scroll', () => {
+  const scrollPosition = container.scrollTop;
+  const totalHeight = container.scrollHeight - container.offsetHeight;
+  const scrollPercentage = scrollPosition / totalHeight;
+  indicator.style.top = `${scrollPercentage * 100}%`;
+});
